c33aeff6b93dfe2f8311a80c388d01e3f389a3bb
TomaszP17 <tplucinski15@gmail.com>
2024-04-13 15:04:42 +0200

    Deleted comments

--- ApiApp/models/Animal.cs
+++ ApiApp/models/Animal.cs
@@ -2,12 +2,9 @@ namespace ApiApp.models;
 
 public class Animal(int id, string name, string category, double weight, string colour)
 {
-    //private static int _nextId;
     public int Id { get; set; } = id;
     public string Name { get; set; } = name;
     public string Category { get; set; } = category;
     public double Weight { get; set; } = weight;
     public string Colour { get; set; } = colour;
-
-    //Id = _nextId++;
 }
